338dd194ae428438847e6db84c59bf53b93428eb
Rodrigo Machado Mena <rodrigo_mena_corebiz@whirlpool.com>
2022-12-05 13:29:34 -0300

    #4 - Utilização de props para renderizar diferentes cards

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,9 +7,9 @@ export function Home() {
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome"/>
       <button type="button">Adicionar</button>
-      <Card />
-      <Card />
-      <Card />
+      
+      <Card name="Rodrigo" time="10:55"/>
+      <Card name="Carlos" time="13:00"/>
     </div>
   )
 }
