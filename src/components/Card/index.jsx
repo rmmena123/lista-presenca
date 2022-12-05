17431d49983717e1c7b0e0c1e07a6109708da120
Rodrigo Machado Mena <rodrigo_mena_corebiz@whirlpool.com>
2022-12-05 13:22:26 -0300

    #3 - Componentes e páginas

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export function Card() {
+    return(
+        <div className="card">
+            <strong>Rodrigo Machado Mena</strong>
+            <small>10:15:23</small>
+        </div>
+    )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css';
+import { Card } from '../../components/Card';
+
+export function Home() {
+  return (
+    <div className="container">
+      <h1>Lista de Presença</h1>
+      <input type="text" placeholder="Digite o nome"/>
+      <button type="button">Adicionar</button>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
